59bf2ba39704860264943ac7dbf9005f761fd2e7
sarikamahboob <sarika.mahboob1234@gmail.com>
2022-05-24 21:45:09 +0600

    payment done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -51,6 +52,18 @@ async function run() {
         res.status(403).send({ message: "forbidden" });
       }
     };
+
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
 
     app.get("/parts", async (req, res) => {
       const query = req.query;
@@ -198,6 +211,13 @@ async function run() {
       } else {
         res.status(403).send({ message: "Forbidden Access" });
       }
+    });
+
+    app.get("/orders/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.findOne(query);
+      res.send(result);
     });
 
     app.delete("/orders/:id", async (req, res) => {
